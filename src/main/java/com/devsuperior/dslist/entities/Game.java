bc1d1b6bb3b8ac3e58a677ddee125617b95f86dc
polly <pollykeller10@gmail.com>
2025-08-18 23:26:18 -0300

    add application properties, test configuration, and Game entity class

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,117 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity //configura a classe como se fosse uma tabela no banco de dados relacional
+@Table(name = "tb_game") //configura o nome da tabela no banco de dados
+public class Game {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY) //indica que o campo id é a chave primária e que seu valor será incrementado automaticamente
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year") //customiza o nome da coluna no banco de dados (palavra reservada year)
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game() {
+    }
+    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+    //para comparar um objeto com outro, é necessário sobrescrever os métodos hashCode e equals
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Game other = (Game) obj;
+        return Objects.equals(id, other.id);
+    }
+}
